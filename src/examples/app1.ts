25513105ee29b448d1372ed7577524b052afd17b
Jeffrey Yang <jeffreyhasmail@gmail.com>
2020-10-01 16:33:16 -0700

    code samples

--- src/examples/app1.ts
+++ src/examples/app1.ts
@@ -1,15 +1,21 @@
-import nn from 'jeffs_frontend_lib';
+import nn from "jeffs_frontend_lib";
+import { ex1JS } from "../codeHTML/js.json";
+import { ex1HTML } from "../codeHTML/html.json";
 export default (attachElSelector: string) => {
   const app = new nn({
     el: attachElSelector,
     data: {
-      exData: 'exData',
-      description: `Here's a simple example: this instance has been made available globally
-      as 'reactive1'. The HTML content below is tied to the property 'exData'. You can try modifying
-      that data property by reassigning reactive1.state.exData. If you look at the state, you'll
-      also find that this description is also a property on the state, which can also be changed!`
-    }
+      exData: "initial data value",
+      description: `Here's a simple example: this $NAME instance has been made available 
+      globally as 'reactive1'. The purple text below is tied to the property 'exData'. 
+      You can try modifying that data property by reassigning reactive1.state.exData in 
+      your console. If you look at the state, you'll also find that this description is 
+      a property on the state, which can also be changed.`,
+      jsSample: ex1JS,
+      htmlSample: ex1HTML,
+    },
   });
   //@ts-ignore
   window.reactive1 = app;
+  console.log(ex1JS);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
 import nn from "jeffs_frontend_lib";
-import app1 from './examples/app1';
-app1('#ex1');
-
+import app1 from "./examples/app1";
+app1("#ex1");
 const desc = new nn({
-  el: '#desc',
+  el: "#desc",
   data: {
-    title: 'My Frontend Library',
+    title: "My Frontend Library",
     pars: [
       `This is a frontend micro-library focusing on data reactivity
       and HTML data binding.`,
       `$NAME gives a small subset of the functionality frameworks like React, 
       Angular, and Vue provide while also being less efficient and more infinite-loop
       prone.`,
-      `It's very lightweight: 4kb min + gzip. $NAME is written entirely in TypeScript with
-      no runtime dependencies.`
+      `It's very lightweight: 4kb min + gzip. $NAME is written using TypeScript, with
+      no runtime dependencies.`,
     ],
     features: [
-      'Reactive Data',
-      'Computed Properties',
-      'Property Watchers',
-      'Two-Way input binding',
-      'Declarative, reactive HTML list binding'
-    ]
-  }
+      "Reactive Data",
+      "Computed Properties",
+      "Property Watchers",
+      "Two-Way input binding",
+      "Declarative, reactive HTML list binding",
+    ],
+  },
 });
-
-
