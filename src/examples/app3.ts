928328bbd990ed6416a6ef25c6691d8697082699
Jeffrey Yang <jeffrey@pop-os.localdomain>
2020-10-08 15:52:23 -0700

    computed and watch samples

--- src/examples/app3.ts
+++ src/examples/app3.ts
@@ -1,11 +1,41 @@
 import nn from "jeffs_frontend_lib";
 import app3TextContent from "../textContent/app3.json";
+import js from "../codeHTML/js.json";
+import html from "../codeHTML/html.json";
 export default (attachElSelector: string) => {
   const app = new nn({
     el: attachElSelector,
     data: {
-      ...app3TextContent
-    }
+      ...app3TextContent,
+      width: 40,
+      height: 40,
+      color: "purple",
+      jsSample: js.computedJS,
+      htmlSample: html.computedHTML,
+    },
+    computed: {
+      area: {
+        fn() {
+          return this.state.width * this.state.height;
+        },
+        dependencies: ["width", "height"],
+      },
+      boxDescription: {
+        fn() {
+          const ts = this.state;
+          return `Height: ${ts.height}px, Width: ${ts.width}px, Area: ${ts.area}px, Color: ${ts.color}`;
+        },
+        dependencies: ["area", "color"],
+      },
+    },
+    watch: {
+      boxDescription() {
+        const box = this.$el.querySelector(".box");
+        box.style.width = `${this.state.width}px`;
+        box.style.height = `${this.state.height}px`;
+        box.style.backgroundColor = this.state.color;
+      },
+    },
   });
   //@ts-ignore
   window.computedExample = app;
--- src/examples/watch.ts
+++ src/examples/watch.ts
@@ -1,22 +1,29 @@
 import nn from "jeffs_frontend_lib";
 import watchTextContent from "../textContent/watch.json";
+import js from "../codeHTML/js.json";
+import html from "../codeHTML/html.json";
 export default (attachElSelector: string) => {
   const app = new nn({
     el: attachElSelector,
     data: {
       ...watchTextContent,
-      currentRoll: 'none'
+      currentRoll: "none",
+      lastRoll: "none",
+      watchJS: js.watchJS,
+      watchHTML: html.watchHTML,
     },
     watch: {
-      currentRoll: function() {
-
-      }
-    }
+      currentRoll: function (lastRoll) {
+        this.state.lastRoll = lastRoll;
+      },
+    },
   });
-  //@ts-ignore
-  // lastRoll currRoll
-  window.watchExample = app;
-  app.$el.querySelector('button').addEventListener('click', () => {
+  // you can access the the attached base element with the $el property.
+  // why is there a $ preceding? Idk it looked cool
+  app.$el.querySelector("button").addEventListener("click", () => {
     app.state.currentRoll = Math.ceil(Math.random() * 6);
   });
+
+  //@ts-ignore
+  window.watchExample = app;
 };
