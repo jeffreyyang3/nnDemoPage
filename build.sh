bbf556f82e63e6135fa063e616f51b0ba9d048d1
Jeffrey Yang <jeffrey@pop-os.localdomain>
2020-10-08 15:59:38 -0700

    change dist branch name

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cp ../index.html . &&
 git init &&
 git add -A &&
 git commit -m "deploy" &&
-git push -f git@github.com:jeffreyyang3/nnDemoPage.git master:gh_pages
+git push -f git@github.com:jeffreyyang3/nnDemoPage.git master:gh-pages
