f8ccc1b7a13c41fdf609148569a2ca226b8d5478
Jeffrey Yang <jeffrey@pop-os.localdomain>
2020-10-07 21:04:23 -0700

    text and some functionality for watch/comp

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import nn from "jeffs_frontend_lib";
 import app1 from "./examples/app1";
 import app2 from "./examples/app2";
 import app3 from "./examples/app3";
+import watch from "./examples/watch";
 import intro from "./textContent/intro.json";
 const t0 = performance.now();
 app1("#ex1");
 app2("#ex2");
 app3("#ex3");
+watch("#ex4");
 const desc = new nn({
   el: "#desc",
   data: intro
